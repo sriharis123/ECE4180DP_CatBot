c59c1501e6052c842dcc628ca20ad6eedee987b4
ssubramanian87 <ssubramanian87@gatech.edu>
2021-12-13 17:00:30 -0500

    Video feed works

---
+++ CatBot/CatBot_MBED_testing_talk.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <termios.h>
+ 
+int main(int argc, char ** argv) {
+  int fd;
+  
+  // Open the Port. We want read/write, no "controlling tty" status, 
+  // and open it no matter what state DCD is in
+  fd = open("/dev/ttyACM0", O_RDWR | O_NOCTTY | O_NDELAY);  
+  //open mbed's USB virtual com port
+  if (fd == -1) {  
+    perror("open_port: Unable to open /dev/ttyACM0 - ");
+    return(-1);
+  }
+  // Turn off blocking for reads, use (fd, F_SETFL, FNDELAY) if you want that
+  fcntl(fd, F_SETFL, 0);
+  //Linux Raw serial setup options
+  struct termios options; 
+  tcgetattr(fd,&options);   //Get current serial settings in structure
+  cfsetspeed(&options, B9600);   //Change a only few
+  options.c_cflag &= ~CSTOPB;
+  options.c_cflag |= CLOCAL;
+  options.c_cflag |= CREAD;
+  cfmakeraw(&options);
+  tcsetattr(fd,TCSANOW,&options);    //Set serial to new settings
+  sleep(1);
+  
+  while(1) 
+  {
+      char control;
+      printf("\nEnter command: ");
+      scanf("%c", &control);
+      write(fd, &control, 1);
+      sleep(1);
+      
+  }
+  // Don't forget to clean up and close the port
+  tcdrain(fd);
+  close(fd);
+  return 0;
+}
+ 
